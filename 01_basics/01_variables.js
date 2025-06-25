1155a6754acdcc6f8f6015e279fb539f822ada77
Shreyansmodi7 <shreyansmodi719@gmail.com>
2025-06-25 11:35:23 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 133445
+let accountEmail = "sm2570@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+/*
+Prefer not to use var*/
+//accountId = 45689 constant cannot be changed
+
+
+console.log(accountId);
+
+console.table([accountId , accountEmail , accountPassword ,accountCity ,accountState ])
