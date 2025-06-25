0095f549484f86ee1260a1fe2fd44bd47f30bbbd
Shreyansmodi7 <shreyansmodi719@gmail.com>
2025-06-25 11:26:56 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict "//treat all js code as newer version
+
+console.log(3 + 3)
+console.log("Shreyans")
+
+/* number => 2 to power 53
+big int 
+string => ""
+boolean => true/false
+null => standalone value 
+sumbol => unique 
+*/
+
+ console.log(typeof "Shreyans");
+ 
+
